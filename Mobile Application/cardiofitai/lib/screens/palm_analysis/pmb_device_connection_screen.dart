c4afb7ab19cd90d5271aff6843ee54ee52b92f63
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-03-19 20:36:27 +0530

    Created pmb_device_connection_screen.dart

---
+++ Mobile Application/cardiofitai/lib/screens/palm_analysis/pmb_device_connection_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class PMBDeviceConnectionScreen extends StatefulWidget {
+  const PMBDeviceConnectionScreen({super.key});
+
+  @override
+  State<PMBDeviceConnectionScreen> createState() => _PMBDeviceConnectionScreenState();
+}
+
+class _PMBDeviceConnectionScreenState extends State<PMBDeviceConnectionScreen> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+    );
+  }
+}
