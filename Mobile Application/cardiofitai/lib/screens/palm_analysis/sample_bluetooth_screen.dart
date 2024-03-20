c9c316d76fa4f9125b6823c6c1ace4e3d0c8b373
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-03-20 19:32:52 +0530

    Created sample_bluetooth_screen.dart

---
+++ Mobile Application/cardiofitai/lib/screens/palm_analysis/sample_bluetooth_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SampleBluetoothScreen extends StatefulWidget {
+  const SampleBluetoothScreen({super.key});
+
+  @override
+  State<SampleBluetoothScreen> createState() => _SampleBluetoothScreenState();
+}
+
+class _SampleBluetoothScreenState extends State<SampleBluetoothScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
