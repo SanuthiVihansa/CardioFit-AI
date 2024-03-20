14eeccbe2d55f35a4a5da61c389a67e519283308
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-03-20 19:21:32 +0530

    Finalized pmb_device_connection_screen.dart

--- Mobile Application/cardiofitai/lib/screens/palm_analysis/pmb_device_connection_screen.dart
+++ Mobile Application/cardiofitai/lib/screens/palm_analysis/pmb_device_connection_screen.dart
@@ -10,8 +10,8 @@ class PMBDeviceConnectionScreen extends StatefulWidget {
 }
 
 class _PMBDeviceConnectionScreenState extends State<PMBDeviceConnectionScreen> {
-  double _devDeviceWidth = 1280.0;
-  double _devDeviceHeight = 720.0;
+  final double _devDeviceWidth = 1280.0;
+  final double _devDeviceHeight = 720.0;
 
   @override
   void initState() {
@@ -23,37 +23,42 @@ class _PMBDeviceConnectionScreenState extends State<PMBDeviceConnectionScreen> {
 
   @override
   Widget build(BuildContext context) {
-    double _width = MediaQuery.of(context).size.width;
-    double _height = MediaQuery.of(context).size.height;
-
-    print(_width);
-    print(_height);
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(
-            width: _width / (_devDeviceWidth / 700),
-            // width: 700,
-            child: Text(
-              "Please connect the Ambulatory ECG Device to continue...",
-              style: TextStyle(color: Colors.red, fontSize: 40),
-              textAlign: TextAlign.center,
+          Padding(
+            padding:
+                EdgeInsets.only(bottom: height / (_devDeviceHeight / 50.0)),
+            child: SizedBox(
+              width: width / (_devDeviceWidth / 700),
+              // width: 700,
+              child: Text(
+                "Please connect the Ambulatory ECG Device to continue...",
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: height / (_devDeviceHeight / 40)),
+                textAlign: TextAlign.center,
+              ),
             ),
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Image.asset(
                 'assets/palm_analysis/tablet.png',
-                width: 600.0,
-                height: 240.0,
+                width: width / (_devDeviceWidth / 600),
+                height: height / (_devDeviceHeight / 240),
                 fit: BoxFit.contain,
               ),
               Image.asset(
                 'assets/palm_analysis/usb.png',
-                width: 600.0,
-                height: 240.0,
+                width: width / (_devDeviceWidth / 600),
+                height: height / (_devDeviceHeight / 240),
                 fit: BoxFit.contain,
               ),
             ],
