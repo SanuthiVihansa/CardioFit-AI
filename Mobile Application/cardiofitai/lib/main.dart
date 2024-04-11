70cb390f6154af4354822c25054945688761bca0
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-04-11 15:40:05 +0530

    Fixed firebase issues

--- Mobile Application/cardiofitai/lib/main.dart
+++ Mobile Application/cardiofitai/lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:cardiofitai/screens/firebase_testing/testing_firebase_screen.dart';
 import 'package:cardiofitai/screens/palm_analysis/pmb_device_connection_screen.dart';
 import 'package:cardiofitai/screens/palm_analysis/bluetooth_testing/sample_bluetooth_screen.dart';
 import 'package:cardiofitai/screens/palm_analysis/real_time_record.dart';
-import 'package:cardiofitai/screens/testing_firebase/testing_firebase_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
 
 // import 'firebase_options.dart';
 
@@ -11,7 +13,7 @@ import 'package:firebase_core/firebase_core.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    // options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const CardioFitAi());
 }
