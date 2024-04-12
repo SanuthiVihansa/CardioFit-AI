9d416f8233ac5645a5f028fcdc3098ab553dfb30
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-04-12 14:54:43 +0530

    Added pick file method

--- Mobile Application/cardiofitai/lib/screens/palm_analysis/file_selecting_screen.dart
+++ Mobile Application/cardiofitai/lib/screens/palm_analysis/file_selecting_screen.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -10,13 +11,28 @@ class FileSelectingScreen extends StatefulWidget {
 }
 
 class _FileSelectingScreenState extends State<FileSelectingScreen> {
-
   @override
   void initState() {
     super.initState();
 
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+  }
+
+  void _pickFile() async {
+    // opens storage to pick files and the picked file or files
+    // are assigned into result and if no file is chosen result is null.
+    // you can also toggle "allowMultiple" true or false depending on your need
+    final result = await FilePicker.platform.pickFiles(allowMultiple: false);
+
+    // if no file is picked
+    if (result == null) return;
+
+    // we will log the name, size and path of the
+    // first picked file (if multiple are selected)
+    print(result.files.first.name);
+    print(result.files.first.size);
+    print(result.files.first.path);
   }
 
   @override
@@ -29,7 +45,9 @@ class _FileSelectingScreenState extends State<FileSelectingScreen> {
       body: Center(
           child: ElevatedButton(
         child: const Text("Select file"),
-        onPressed: () {},
+        onPressed: () {
+          _pickFile();
+        },
       )),
     );
   }
