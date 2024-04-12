7326f513deda39ec324e2e6a6cd38e8336700434
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-04-12 23:09:48 +0530

    Removed unwanted import statement

--- Mobile Application/cardiofitai/lib/screens/palm_analysis/file_selecting_screen.dart
+++ Mobile Application/cardiofitai/lib/screens/palm_analysis/file_selecting_screen.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
-
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
