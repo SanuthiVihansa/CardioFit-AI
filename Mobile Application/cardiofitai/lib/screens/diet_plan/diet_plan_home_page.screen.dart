38e6e8a40088e01404c7d7800f7d2a8eb0645d3f
SanuthiVihansa <subasofts@gmail.com>
2024-04-13 17:51:37 +0530

    Commit user profile page changes

---
+++ Mobile Application/cardiofitai/lib/screens/diet_plan/diet_plan_home_page.screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DietHomePage extends StatefulWidget {
+  const DietHomePage({super.key});
+
+  @override
+  State<DietHomePage> createState() => _DietHomePageState();
+}
+
+class _DietHomePageState extends State<DietHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
