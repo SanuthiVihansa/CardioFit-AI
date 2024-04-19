890df887de98613e0e233c7cff27875e6ff09d8c
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-04-19 16:24:13 +0530

    Added calcMinMax method and ECG Plot

--- Mobile Application/cardiofitai/lib/screens/palm_analysis/file_selecting_screen.dart
+++ Mobile Application/cardiofitai/lib/screens/palm_analysis/file_selecting_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:file_picker/file_picker.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
@@ -12,12 +13,18 @@ class FileSelectingScreen extends StatefulWidget {
 }
 
 class _FileSelectingScreenState extends State<FileSelectingScreen> {
+  late List<double> _tenSecData = [];
+  double _maxValue = 0;
+  double _minValue = 0;
+
   @override
   void initState() {
     super.initState();
 
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+
+    print(_tenSecData.length);
   }
 
   void _pickFile() async {
@@ -34,11 +41,70 @@ class _FileSelectingScreenState extends State<FileSelectingScreen> {
       return double.tryParse(value) ?? 0.0;
     }).toList();
 
-    List<double> tenSecData = dataList.sublist(0, 2560);
+    _tenSecData = dataList.sublist(0, 2560);
+    _calcMinMax(_tenSecData);
+    setState(() {});
 
-    print(tenSecData);
+    // print(_tenSecData);
 
     await DefaultCacheManager().emptyCache();
+  }
+
+  void _calcMinMax(List<double> data) {
+    _minValue =
+        data.reduce((value, element) => value < element ? value : element) -
+            0.5;
+    _maxValue =
+        data.reduce((value, element) => value > element ? value : element) +
+            0.5;
+  }
+
+  Widget _ecgPlot() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: LineChart(
+        LineChartData(
+          lineBarsData: [
+            LineChartBarData(
+              spots: List.generate(
+                _tenSecData.length,
+                (index) => FlSpot(index.toDouble(), _tenSecData[index]),
+              ),
+              isCurved: false,
+              colors: [Colors.blue],
+              barWidth: 2,
+              isStrokeCapRound: true,
+              dotData: FlDotData(
+                show: false,
+              ),
+            ),
+          ],
+          minY: _minValue,
+          // Adjust these values based on your data range
+          maxY: _maxValue,
+          titlesData: FlTitlesData(
+            bottomTitles: SideTitles(
+              showTitles: true,
+              getTitles: (value) {
+                return value.toInt().toString();
+              },
+            ),
+            leftTitles: SideTitles(
+              showTitles: true,
+            ),
+          ),
+          borderData: FlBorderData(
+            show: true,
+            border: Border.all(color: Colors.black),
+          ),
+          gridData: FlGridData(
+            show: true,
+            drawHorizontalLine: true,
+            drawVerticalLine: true,
+          ),
+        ),
+      ),
+    );
   }
 
   @override
@@ -49,12 +115,14 @@ class _FileSelectingScreenState extends State<FileSelectingScreen> {
         backgroundColor: Colors.red,
       ),
       body: Center(
-          child: ElevatedButton(
-        child: const Text("Select file"),
-        onPressed: () {
-          _pickFile();
-        },
-      )),
+          child: _tenSecData.length == 0
+              ? ElevatedButton(
+                  child: const Text("Select file"),
+                  onPressed: () {
+                    _pickFile();
+                  },
+                )
+              : _ecgPlot()),
     );
   }
 }
