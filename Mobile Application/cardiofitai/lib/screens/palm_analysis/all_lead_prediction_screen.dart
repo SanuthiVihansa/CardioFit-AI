5b61c78285ae397c6d5043c080282ccec5184373
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-04-25 17:14:04 +0530

    Added all leads

--- Mobile Application/cardiofitai/lib/screens/palm_analysis/all_lead_prediction_screen.dart
+++ Mobile Application/cardiofitai/lib/screens/palm_analysis/all_lead_prediction_screen.dart
@@ -126,6 +126,26 @@ class _AllLeadPredictionScreenState extends State<AllLeadPredictionScreen> {
           decodedData["l1"].map((element) => element.toDouble()));
       _l2Data = List<double>.from(
           decodedData["l2"].map((element) => element.toDouble()));
+      _l3Data = List<double>.from(
+          decodedData["l3"].map((element) => element.toDouble()));
+      _avrData = List<double>.from(
+          decodedData["avr"].map((element) => element.toDouble()));
+      _avlData = List<double>.from(
+          decodedData["avl"].map((element) => element.toDouble()));
+      _avfData = List<double>.from(
+          decodedData["avf"].map((element) => element.toDouble()));
+      _v1Data = List<double>.from(
+          decodedData["v1"].map((element) => element.toDouble()));
+      _v2Data = List<double>.from(
+          decodedData["v2"].map((element) => element.toDouble()));
+      _v3Data = List<double>.from(
+          decodedData["v3"].map((element) => element.toDouble()));
+      _v4Data = List<double>.from(
+          decodedData["v4"].map((element) => element.toDouble()));
+      _v5Data = List<double>.from(
+          decodedData["v5"].map((element) => element.toDouble()));
+      _v6Data = List<double>.from(
+          decodedData["v6"].map((element) => element.toDouble()));
 
       print(decodedData["l2"].runtimeType);
     } else {
@@ -148,12 +168,52 @@ class _AllLeadPredictionScreenState extends State<AllLeadPredictionScreen> {
                   children: [
                     SizedBox(
                         height: 200,
-                        child: _ecgPlot(_l1Data, _calcMin(_l1Data),
-                            _calcMax(_l1Data))),
+                        child: _ecgPlot(
+                            _l1Data, _calcMin(_l1Data), _calcMax(_l1Data))),
                     SizedBox(
                         height: 200,
                         child: _ecgPlot(
                             _l2Data, _calcMin(_l2Data), _calcMax(_l2Data))),
+                    SizedBox(
+                        height: 200,
+                        child: _ecgPlot(
+                            _l3Data, _calcMin(_l3Data), _calcMax(_l3Data))),
+                    SizedBox(
+                        height: 200,
+                        child: _ecgPlot(
+                            _avrData, _calcMin(_avrData), _calcMax(_avrData))),
+                    SizedBox(
+                        height: 200,
+                        child: _ecgPlot(
+                            _avlData, _calcMin(_avlData), _calcMax(_avlData))),
+                    SizedBox(
+                        height: 200,
+                        child: _ecgPlot(
+                            _avfData, _calcMin(_avfData), _calcMax(_avfData))),
+                    SizedBox(
+                        height: 200,
+                        child: _ecgPlot(
+                            _v1Data, _calcMin(_v1Data), _calcMax(_v1Data))),
+                    SizedBox(
+                        height: 200,
+                        child: _ecgPlot(
+                            _v2Data, _calcMin(_v2Data), _calcMax(_v2Data))),
+                    SizedBox(
+                        height: 200,
+                        child: _ecgPlot(
+                            _v3Data, _calcMin(_v3Data), _calcMax(_v3Data))),
+                    SizedBox(
+                        height: 200,
+                        child: _ecgPlot(
+                            _v4Data, _calcMin(_v4Data), _calcMax(_v4Data))),
+                    SizedBox(
+                        height: 200,
+                        child: _ecgPlot(
+                            _v5Data, _calcMin(_v5Data), _calcMax(_v5Data))),
+                    SizedBox(
+                        height: 200,
+                        child: _ecgPlot(
+                            _v6Data, _calcMin(_v6Data), _calcMax(_v6Data))),
                     ElevatedButton(onPressed: () {}, child: const Text("Back"))
                   ],
                 ),
