dd890becc3334f51de87e07e6390a832ff9e0fcd
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-04-25 23:13:58 +0530

    Added loading text

--- Mobile Application/cardiofitai/lib/screens/palm_analysis/all_lead_prediction_screen.dart
+++ Mobile Application/cardiofitai/lib/screens/palm_analysis/all_lead_prediction_screen.dart
@@ -272,7 +272,18 @@ class _AllLeadPredictionScreenState extends State<AllLeadPredictionScreen> {
                 ],
               )
             : _resCode == 0
-                ? Center(child: CircularProgressIndicator())
+                ? Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircularProgressIndicator(),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10.0),
+                          child: Text("Loading..."),
+                        )
+                      ],
+                    ),
+                  )
                 : Center(
                     child: Text("Error"),
                   ));
