32be9c520a83b71669d84921e1f80bad13c6856c
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-04-25 23:07:25 +0530

    Added necessary code for ecg_monitoring_home_screen.dart

--- Mobile Application/cardiofitai/lib/screens/common/ecg_monitoring_home_screen.dart
+++ Mobile Application/cardiofitai/lib/screens/common/ecg_monitoring_home_screen.dart
@@ -1,15 +1,51 @@
+import 'package:cardiofitai/screens/palm_analysis/file_selection_screen.dart';
 import 'package:flutter/material.dart';
 
 class ECGMonitoringHomeScreen extends StatefulWidget {
   const ECGMonitoringHomeScreen({super.key});
 
   @override
-  State<ECGMonitoringHomeScreen> createState() => _ECGMonitoringHomeScreenState();
+  State<ECGMonitoringHomeScreen> createState() =>
+      _ECGMonitoringHomeScreenState();
 }
 
 class _ECGMonitoringHomeScreenState extends State<ECGMonitoringHomeScreen> {
+  void _onClickFacialAnalysisBtn() {}
+
+  void _onClickPalmAnalysisBtn() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (BuildContext context) => FileSelectionScreen()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "ECG Monitoring",
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  _onClickFacialAnalysisBtn();
+                },
+                child: Text("Facial Analysis")),
+            ElevatedButton(
+                onPressed: () {
+                  _onClickPalmAnalysisBtn();
+                },
+                child: Text("Palm Analysis"))
+          ],
+        ),
+      ),
+    );
   }
 }
