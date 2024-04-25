731f45130233c74df66c077f00a42fbeadd3b131
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-04-25 22:16:35 +0530

    Created ecg_monitoring_home_screen.dart

---
+++ Mobile Application/cardiofitai/lib/screens/common/ecg_monitoring_home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ECGMonitoringHomeScreen extends StatefulWidget {
+  const ECGMonitoringHomeScreen({super.key});
+
+  @override
+  State<ECGMonitoringHomeScreen> createState() => _ECGMonitoringHomeScreenState();
+}
+
+class _ECGMonitoringHomeScreenState extends State<ECGMonitoringHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
