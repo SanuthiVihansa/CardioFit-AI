b5b0890916cc8de4487333377e30b72d89b797c3
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-04-26 13:05:07 +0530

    Added password and type fields

--- Mobile Application/cardiofitai/lib/models/user.dart
+++ Mobile Application/cardiofitai/lib/models/user.dart
@@ -1,12 +1,14 @@
 class User{
   String name;
   String email;
+  String password;
   String age;
   String height;
   String weight;
   String phone;
+  String type;
 
 
-  User(this.name, this.email, this.age, this.height, this.weight,
-      this.phone);
+  User(this.name, this.email, this.password, this.age, this.height, this.weight,
+      this.phone, this.type);
 }
