a591c037edf22ac066d6212a4ea1ace5ebd7bae3
Hilarina <111520051+Hilarina@users.noreply.github.com>
2024-05-07 20:30:05 +0530

    Changes added to new branch

--- Mobile Application/cardiofitai/lib/screens/defect_prediction/ecg_plot.dart
+++ Mobile Application/cardiofitai/lib/screens/defect_prediction/ecg_plot.dart
@@ -17,8 +17,6 @@ class ECGDiagnosisScreen extends StatefulWidget {
 }
 
 class _ECGDiagnosisScreenState extends State<ECGDiagnosisScreen> {
-
-
   File? _selectedFile;
   String _predictedLabel = '';
   List<double> _ecgData = [];
@@ -81,17 +79,22 @@ class _ECGDiagnosisScreenState extends State<ECGDiagnosisScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ECG Diagnosis'),
+        foregroundColor: Colors.white,
+        title: const Text(
+          "ECG Diagnosis",
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        backgroundColor: Colors.red,
       ),
       body: Center(
-        child: _ecgData.isNotEmpty
+        child: _predictedLabel.isNotEmpty
             ? Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Padding(
               padding: EdgeInsets.all(20.0),
               child: Text(
-                'Predicted Label: $_predictedLabel',
+                'Diagnosis: $_predictedLabel',
                 style: TextStyle(fontSize: 20),
               ),
             ),
@@ -136,8 +139,21 @@ class _ECGDiagnosisScreenState extends State<ECGDiagnosisScreen> {
             ),
           ],
         )
-            : CircularProgressIndicator(), // Show loading indicator while processing
+            : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                'Diagnosis of Arrhythmias In Progress',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            CircularProgressIndicator(), // Show loading indicator while processing
+          ],
+        ),
       ),
     );
   }
 }
+
