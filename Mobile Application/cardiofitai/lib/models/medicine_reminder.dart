d14f19e48ee2fd9e8c40658597969d96da19917f
SanuthiVihansa <subasofts@gmail.com>
2024-06-09 15:08:04 +0530

    model created for medicine reminder

---
+++ Mobile Application/cardiofitai/lib/models/medicine_reminder.dart
@@ -0,0 +1,11 @@
+class MedicalReminder{
+  String medicineName;
+  String dosage;
+  String prescribedPeriod;
+  double numberOfPills;
+  String additionalInfo;
+  String startDate;
+  String startTime;
+
+  MedicalReminder(this.medicineName,this.dosage,this.prescribedPeriod,this.numberOfPills,this.additionalInfo,this.startDate,this.startTime);
+}
