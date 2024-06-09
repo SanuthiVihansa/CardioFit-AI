1ca3ea9404ce1eec463f4d22ee02c001ed20c55d
SanuthiVihansa <subasofts@gmail.com>
2024-06-09 17:32:25 +0530

    service function created

--- Mobile Application/cardiofitai/lib/services/medicineReminderService.dart
+++ Mobile Application/cardiofitai/lib/services/medicineReminderService.dart
@@ -1,24 +1,42 @@
-import 'package:cardiofitai/models/response.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/response.dart';
+import '../models/medicine_reminder.dart';
 
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 final CollectionReference medicineReminderCollectionReference =
     _firestore.collection("MedicineReminder");
 
 class MedicineReminderService {
+  //Get MedicineReminder
+  static Future<QuerySnapshot<Object?>> getAllUserReminder(String email) async {
+    return await medicineReminderCollectionReference
+        .where("userEmail", isEqualTo: email)
+        .get();
+  }
+
+  // Get specific Reminder of specific User
+  static Future<QuerySnapshot<Object?>> getUserReminder(
+      String email, int reminderNo) async {
+    return await medicineReminderCollectionReference
+        .where("userEmail", isEqualTo: email)
+        .where("reminderNo", isEqualTo: reminderNo)
+        .get();
+  }
+
   //create medicineReminder
   static Future<Response> medicineReminder(
-      int reminderNo,
-      String userEmail,
-      String medicineName,
-      String dosage,
-      String pillIntake,
-      String interval,
-      int days,
-      String startDate,
-      String additionalInstructions,
-      List<String> daysOfWeek,
-      String startTime) async {
+    int reminderNo,
+    String userEmail,
+    String medicineName,
+    String dosage,
+    String pillIntake,
+    String interval,
+    int days,
+    String startDate,
+    String additionalInstructions,
+    List<String> daysOfWeek,
+    String startTime,
+  ) async {
     Response response = Response();
     Map<String, dynamic> data = <String, dynamic>{
       "reminderNo": reminderNo,
@@ -31,7 +49,7 @@ class MedicineReminderService {
       "startDate": startDate,
       "additionalInstructions": additionalInstructions,
       "daysOfWeek": daysOfWeek,
-      "startTime": startTime
+      "startTime": startTime,
     };
 
     await medicineReminderCollectionReference.doc().set(data).whenComplete(() {
@@ -39,8 +57,51 @@ class MedicineReminderService {
       response.message = "Reminder Saved";
     }).catchError((onError) {
       response.code = 500;
-      response.message = onError;
+      response.message = onError.toString();
     });
+    return response;
+  }
+
+  // Update a created medicine reminder
+  static Future<Response> updateReminder(
+      MedicalReminder medicalReminder) async {
+    Response response = Response();
+
+    try {
+      QuerySnapshot querySnapshot = await medicineReminderCollectionReference
+          .where("userEmail", isEqualTo: medicalReminder.userEmail)
+          .where("reminderNo", isEqualTo: medicalReminder.reminderNo)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        QueryDocumentSnapshot document = querySnapshot.docs[0];
+
+        Map<String, dynamic> data = <String, dynamic>{
+          "reminderNo": medicalReminder.reminderNo,
+          "userEmail": medicalReminder.userEmail,
+          "medicineName": medicalReminder.medicineName,
+          "dosage": medicalReminder.dosage,
+          "pillIntake": medicalReminder.pillIntake,
+          "interval": medicalReminder.interval,
+          "days": medicalReminder.days,
+          "startDate": medicalReminder.startDate,
+          "additionalInstructions": medicalReminder.additionalInstructions,
+          "daysOfWeek": medicalReminder.daysOfWeek,
+          "startTime": medicalReminder.startTime,
+        };
+
+        await document.reference.update(data); // Update the document
+        response.code = 200;
+        response.message = "Reminder updated successfully!";
+      } else {
+        response.code = 404;
+        response.message = "Reminder not found";
+      }
+    } catch (e) {
+      response.code = 500;
+      response.message = "Server Error: ${e.toString()}";
+    }
+
     return response;
   }
 }
