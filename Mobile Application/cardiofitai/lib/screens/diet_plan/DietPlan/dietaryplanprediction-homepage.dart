d8311f7b2200b43f227a7d8f3f8c32f9cd7ff36b
SanuthiVihansa <subasofts@gmail.com>
2024-07-17 07:01:37 +0530

    propose diet plan success

--- Mobile Application/cardiofitai/lib/screens/diet_plan/DietPlan/dietaryplanprediction-homepage.dart
+++ Mobile Application/cardiofitai/lib/screens/diet_plan/DietPlan/dietaryplanprediction-homepage.dart
@@ -20,16 +20,32 @@ class _DietaryPlanHomePageState extends State<DietaryPlanHomePage> {
   final TextEditingController bmiController = TextEditingController();
   final TextEditingController cholesterolController = TextEditingController();
   final TextEditingController sugarController = TextEditingController();
-  String gender = '';
-  String cardiacCondition = '';
+  String gender = 'Female';
+  String cardiacCondition = 'No';
   String bmiResult = '';
   String bloodSugarResult = '';
-  String cardiacConditionResult = '';
+  String cardiacConditionResult = 'No';
   String cholesterolResult = '';
   String ageRange = '';
   String advice = '';
 
 //Dictionary key for Diet advice proposal
+//   final Map<String, dynamic> dietAdvice = {
+//     '18-65': {
+//       'Female': {
+//         'Obese': {
+//           'Yes': {
+//             'Normal': {
+//               'Normal': 'Get a balanced diet',
+//               'High': 'Get a balanced diet, Limit high sugary foods',
+//               'Low': 'Get a balanced diet',
+//             },
+//           },
+//         },
+//       },
+//     },
+//   };
+
   final Map<String, dynamic> dietAdvice = {
     '0-19': {
       'Female': {
@@ -1080,7 +1096,6 @@ class _DietaryPlanHomePageState extends State<DietaryPlanHomePage> {
     return 'Unknown';
   }
 
-
   void provideDietAdvice() {
     int age = int.parse(ageController.text);
     String ageRange = '';
@@ -1107,11 +1122,11 @@ class _DietaryPlanHomePageState extends State<DietaryPlanHomePage> {
 
   String categorizeCholesterol(double cholesterol) {
     if (cholesterol < 200) {
-      return 'Desirable';
+      return 'High';
     } else if (cholesterol >= 200 && cholesterol <= 239) {
       return 'Borderline High';
     } else {
-      return 'High';
+      return 'Normal';
     }
   }
 
@@ -1119,10 +1134,11 @@ class _DietaryPlanHomePageState extends State<DietaryPlanHomePage> {
     _userSignUpInfo = await UserLoginService.getUserByEmail(widget.user.email);
     ageController.text = _userSignUpInfo.docs[0]["age"];
     bmiController.text = _userSignUpInfo.docs[0]["bmi"];
-    cholesterolController.text = _userSignUpInfo.docs[0]["bloodCholestrolLevel"];
+    cholesterolController.text =
+        _userSignUpInfo.docs[0]["bloodCholestrolLevel"];
     sugarController.text = _userSignUpInfo.docs[0]["bloodGlucoseLevel"];
-    gender=_userSignUpInfo.docs[0]["gender"];
-    cardiacCondition=_userSignUpInfo.docs[0]["cardiacCondition"];
+    //gender=_userSignUpInfo.docs[0]["gender"];
+    cardiacCondition = _userSignUpInfo.docs[0]["cardiacCondition"];
     setState(() {});
   }
 
@@ -1139,22 +1155,48 @@ class _DietaryPlanHomePageState extends State<DietaryPlanHomePage> {
       double cholesterol = double.tryParse(cholesterolController.text) ?? 0.0;
       cholesterolResult = categorizeCholesterol(cholesterol);
       double sugar = double.tryParse(sugarController.text) ?? 0.0;
-      bloodSugarResult = categorizeBloodSugar(sugar, 'Fasting'); // or 'Fasting', as needed
+      bloodSugarResult = categorizeBloodSugar(sugar, 'Fasting'); // or 'Random', as needed
 
-      try {
-        int age = int.parse(ageController.text);
-        if (age <= 19) {
-          ageRange = '0-19';
-        } else if (age <= 65) {
-          ageRange = '18-65';
+      int age = int.parse(ageController.text);
+      if (age <= 19) {
+        ageRange = '0-19';
+      } else if (age <= 65) {
+        ageRange = '18-65';
+      } else {
+        ageRange = '65+';
+      }
+
+      print('ageRange: $ageRange');
+      print('gender: $gender');
+      print('bmiCategory: $bmiCategory');
+      print('cardiacCondition: $cardiacCondition');
+      print('bloodSugarResult: $bloodSugarResult');
+      print('cholesterolResult: $cholesterolResult');
+
+      if (dietAdvice.containsKey(ageRange)) {
+        if (dietAdvice[ageRange].containsKey(gender)) {
+          if (dietAdvice[ageRange][gender].containsKey(bmiCategory)) {
+            if (dietAdvice[ageRange][gender][bmiCategory].containsKey(cardiacCondition)) {
+              if (dietAdvice[ageRange][gender][bmiCategory][cardiacCondition].containsKey(bloodSugarResult)) {
+                if (dietAdvice[ageRange][gender][bmiCategory][cardiacCondition][bloodSugarResult].containsKey(cholesterolResult)) {
+                  advice = dietAdvice[ageRange][gender][bmiCategory][cardiacCondition][bloodSugarResult][cholesterolResult];
+                } else {
+                  advice = 'No advice available for the given cholesterol result';
+                }
+              } else {
+                advice = 'No advice available for the given blood sugar result';
+              }
+            } else {
+              advice = 'No advice available for the given cardiac condition';
+            }
+          } else {
+            advice = 'No advice available for the given BMI category';
+          }
         } else {
-          ageRange = '65+';
+          advice = 'No advice available for the given gender';
         }
-
-        advice = dietAdvice[ageRange][gender][bmiCategory][cardiacCondition]
-        [bloodSugarResult][cholesterolResult];
-      } catch (e) {
-        advice = 'No advice available for the given inputs';
+      } else {
+        advice = 'No advice available for the given age range';
       }
     });
   }
@@ -1162,172 +1204,183 @@ class _DietaryPlanHomePageState extends State<DietaryPlanHomePage> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    drawer: LeftNavBar(
-        user: widget.user,
-        name: widget.user.name,
-        email: widget.user.email,
-        width: 150,
-        height: 300),
-    appBar: AppBar(
-      title: Align(
-          alignment: Alignment.center,
-          child: Text('Customised Dietary Advice')),
-    ),
-    body: SingleChildScrollView(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('User Information',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-          SizedBox(height: 10),
-          Row(
+        // drawer: LeftNavBar(
+        //     user: widget.user,
+        //     name: widget.user.name,
+        //     email: widget.user.email,
+        //     width: 150,
+        //     height: 300),
+        appBar: AppBar(
+          leading: IconButton(
+            color: Colors.black,
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: Align(
+              alignment: Alignment.center,
+              child: Text('Customised Dietary Advice')),
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Expanded(
-                child: TextField(
-                  controller: ageController,
-                  decoration: InputDecoration(
-                    labelText: 'Age',
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.number,
-                ),
-              ),
-              SizedBox(width: 20),
-              Expanded(
-                child: InputDecorator(
-                  decoration: InputDecoration(
-                    labelText: 'Gender',
-                    contentPadding:
-                    EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                    border: OutlineInputBorder(),
-                  ),
-                  child: DropdownButtonHideUnderline(
-                    child: DropdownButton<String>(
-                      value: gender,
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          gender = newValue!;
-                        });
-                      },
-                      items: <String>['Female', 'Male']
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      isExpanded: true,
+              Text('User Information',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: ageController,
+                      decoration: InputDecoration(
+                        labelText: 'Age',
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.number,
                     ),
                   ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 10),
-          TextField(
-            controller: bmiController,
-            decoration: InputDecoration(
-              labelText: 'BMI (kg/m²)',
-              border: OutlineInputBorder(),
-            ),
-            keyboardType: TextInputType.number,
-          ),
-          SizedBox(height: 10),
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: cholesterolController,
-                  decoration: InputDecoration(
-                    labelText: 'Blood Cholesterol Level',
-                    border: OutlineInputBorder(),
+                  SizedBox(width: 20),
+                  Expanded(
+                    child: InputDecorator(
+                      decoration: InputDecoration(
+                        labelText: 'Gender',
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                        border: OutlineInputBorder(),
+                      ),
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton<String>(
+                          value: gender,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              gender = newValue!;
+                            });
+                          },
+                          items: <String>['Female', 'Male']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          isExpanded: true,
+                        ),
+                      ),
+                    ),
                   ),
-                  keyboardType: TextInputType.number,
-                ),
+                ],
               ),
-              SizedBox(width: 20),
-              Expanded(
-                child: TextField(
-                  controller: sugarController,
-                  decoration: InputDecoration(
-                    labelText: 'Blood Sugar Level',
-                    border: OutlineInputBorder(),
+              SizedBox(height: 10),
+              TextField(
+                controller: bmiController,
+                decoration: InputDecoration(
+                  labelText: 'BMI (kg/m²)',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: cholesterolController,
+                      decoration: InputDecoration(
+                        labelText: 'Blood Cholesterol Level',
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
                   ),
-                  keyboardType: TextInputType.number,
+                  SizedBox(width: 20),
+                  Expanded(
+                    child: TextField(
+                      controller: sugarController,
+                      decoration: InputDecoration(
+                        labelText: 'Blood Sugar Level',
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              InputDecorator(
+                decoration: InputDecoration(
+                  labelText: 'Cardiac Condition',
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                  border: OutlineInputBorder(),
+                ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    value: cardiacCondition,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        cardiacCondition = newValue!;
+                      });
+                    },
+                    items: <String>['No', 'Yes']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    isExpanded: true,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: generatePrediction,
+                  child: Text('Generate'),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text('Dietary Prediction',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(child: Text('BMI')),
+                  Expanded(
+                      child: Text(bmiController.text,
+                          textAlign: TextAlign.center)),
+                  Expanded(child: Text('Cardiac Condition')),
+                  Expanded(
+                      child: Text(cardiacConditionResult,
+                          textAlign: TextAlign.center)),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(child: Text('Blood Sugar Level')),
+                  Expanded(
+                      child:
+                          Text(bloodSugarResult, textAlign: TextAlign.center)),
+                  Expanded(child: Text('Blood Cholesterol Level')),
+                  Expanded(
+                      child:
+                          Text(cholesterolResult, textAlign: TextAlign.center)),
+                ],
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: Text(
+                  advice,
+                  style: TextStyle(
+                      color: Colors.green, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
                 ),
               ),
             ],
           ),
-          SizedBox(height: 10),
-          InputDecorator(
-            decoration: InputDecoration(
-              labelText: 'Cardiac Condition',
-              contentPadding:
-              EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-              border: OutlineInputBorder(),
-            ),
-            child: DropdownButtonHideUnderline(
-              child: DropdownButton<String>(
-                value: cardiacCondition,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    cardiacCondition = newValue!;
-                  });
-                },
-                items: <String>['No', 'Yes']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                isExpanded: true,
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          Center(
-            child: ElevatedButton(
-              onPressed: generatePrediction,
-              child: Text('Generate'),
-            ),
-          ),
-          SizedBox(height: 20),
-          Text('Dietary Prediction',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-          SizedBox(height: 10),
-          Row(
-            children: [
-              Expanded(child: Text('BMI')),
-              Expanded(child: Text(bmiController.text, textAlign: TextAlign.center)),
-              Expanded(child: Text('Cardiac Condition')),
-              Expanded(
-                  child: Text(cardiacConditionResult,
-                      textAlign: TextAlign.center)),
-            ],
-          ),
-          Row(
-            children: [
-              Expanded(child: Text('Blood Sugar Level')),
-              Expanded(
-                  child: Text(bloodSugarResult, textAlign: TextAlign.center)),
-              Expanded(child: Text('Blood Cholesterol Level')),
-              Expanded(
-                  child: Text(cholesterolResult, textAlign: TextAlign.center)),
-            ],
-          ),
-          SizedBox(height: 20),
-          Center(
-            child: Text(
-              advice,
-              style: TextStyle(
-                  color: Colors.green, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
+        ),
+      );
 }
