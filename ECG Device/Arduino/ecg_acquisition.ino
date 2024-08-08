28a1f0883f4bcfbd28bb7f419d0bee538f3528e2
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-08-08 20:57:15 +0530

    Updated to the working code

--- ECG Device/Arduino/ecg_acquisition.ino
+++ ECG Device/Arduino/ecg_acquisition.ino
@@ -1,10 +1,19 @@
 void setup() {
+  // initialize the serial communication:
   Serial.begin(9600);
+  pinMode(10, INPUT); // Setup for leads off detection LO +
+  pinMode(11, INPUT); // Setup for leads off detection LO -
+ 
 }
-
+ 
 void loop() {
-  int sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
-  delay(2); // Sample rate of 500 Hz
+  if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
+    Serial.println('!');
+  }
+  else{
+    // send the value of analog input 0:
+    Serial.println(analogRead(A0));
+  }
+  //Wait for a bit to keep serial data from saturating
+  delay(1);
 }
-
