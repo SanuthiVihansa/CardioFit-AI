b6f51798edbc71ceb6534d4b0671ad910aba083e
Hilarina <hilarinamelani21@gmail.com>
2024-08-10 18:05:15 +0530

    Unwanted codes removed

--- Mobile Application/cardiofitai/lib/screens/defect_prediction/EmergencyDialog.dart
+++ Mobile Application/cardiofitai/lib/screens/defect_prediction/EmergencyDialog.dart
@@ -1,82 +1,8 @@
-// import 'package:flutter/material.dart';
-//
-// class EmergencyDialog extends StatelessWidget {
-//   final String contactNumber;
-//
-//   const EmergencyDialog({Key? key, required this.contactNumber}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Dialog(
-//       shape: RoundedRectangleBorder(
-//         borderRadius: BorderRadius.circular(20.0),
-//       ),
-//       child: Container(
-//         padding: EdgeInsets.all(20.0),
-//         decoration: BoxDecoration(
-//           color: Colors.white,
-//           borderRadius: BorderRadius.circular(20.0),
-//         ),
-//         child: Column(
-//           mainAxisSize: MainAxisSize.min,
-//           children: [
-//             Icon(
-//               Icons.warning,
-//               color: Colors.red,
-//               size: 60,
-//             ),
-//             SizedBox(height: 20),
-//             Text(
-//               'Emergency! Contact Immediately!',
-//               style: TextStyle(
-//                 fontSize: 20,
-//                 fontWeight: FontWeight.bold,
-//                 color: Colors.red,
-//               ),
-//               textAlign: TextAlign.center,
-//             ),
-//             SizedBox(height: 20),
-//             Text(
-//               'Incomplete Right Bundle Branch Block detected. Contact the following number:',
-//               style: TextStyle(fontSize: 16),
-//               textAlign: TextAlign.center,
-//             ),
-//             SizedBox(height: 10),
-//             Text(
-//               contactNumber,
-//               style: TextStyle(
-//                 fontSize: 18,
-//                 fontWeight: FontWeight.bold,
-//                 color: Colors.blue,
-//               ),
-//             ),
-//             SizedBox(height: 20),
-//             ElevatedButton(
-//               style: ElevatedButton.styleFrom(
-//                 backgroundColor: Colors.red,
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(10.0),
-//                 ),
-//               ),
-//               onPressed: () {
-//                 Navigator.of(context).pop();
-//               },
-//               child: Text('OK'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
-
 import '../../services/Location_Service.dart';
  // Import the location service
 
