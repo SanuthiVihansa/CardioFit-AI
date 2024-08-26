436af5aef4bb0f23fa26770694a85100f63a3511
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-08-26 17:01:47 +0530

    Implemented getEcgHistory method

--- Mobile Application/cardiofitai/lib/services/ecg_service.dart
+++ Mobile Application/cardiofitai/lib/services/ecg_service.dart
@@ -51,4 +51,41 @@ class EcgService {
 
     return response;
   }
+
+  // static Future<QuerySnapshot<Object?>> getEcgHistory(String email) async {
+  //   return await ecgCollectionReference
+  //       .where("email", isEqualTo: email)
+  //       .orderBy("datetime", descending: true).s
+  //       .get();
+  // }
+
+  static Future<List<Map<String, dynamic>>> getEcgHistory(String email) async {
+    QuerySnapshot<Object?> snapshot = await ecgCollectionReference
+        .where("email", isEqualTo: email)
+        .orderBy("datetime", descending: true)
+        .get();
+
+    // Map the documents to a list of maps containing only the email and datetime fields
+    List<Map<String, dynamic>> ecgHistory = snapshot.docs.map((doc) {
+      var data = doc.data() as Map<String, dynamic>;
+      return {
+        'email': data['email'],
+        "l1": data['l1'],
+        "l2": data['l2'],
+        "l3": data['l3'],
+        "avr": data['avr'],
+        "avl": data['avl'],
+        "avf": data['avf'],
+        "v1": data['v1'],
+        "v2": data['v2'],
+        "v3": data['v3'],
+        "v4": data['v4'],
+        "v5": data['v5'],
+        "v6": data['v6'],
+        'datetime': data['datetime'],
+      };
+    }).toList();
+
+    return ecgHistory;
+  }
 }
