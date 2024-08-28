ebbd3da475c2c4f5d99ddba62209360c301be037
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-08-28 22:24:32 +0530

    Added recent ecg plot to ecg menu

--- Mobile Application/cardiofitai/lib/screens/common/ecg_monitoring_home_screen.dart
+++ Mobile Application/cardiofitai/lib/screens/common/ecg_monitoring_home_screen.dart
@@ -7,8 +7,12 @@ import 'package:cardiofitai/screens/facial_analysis/facial_analysis_home.dart';
 import 'package:cardiofitai/screens/palm_analysis/for_future_use/file_selection_screen.dart';
 import 'package:cardiofitai/screens/palm_analysis/for_pp2/electrode_placement_instructions_screen.dart';
 import 'package:cardiofitai/screens/palm_analysis/temp_file_selection_screen.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+
+import '../../services/ecg_service.dart';
 
 class ECGMonitoringHomeScreen extends StatefulWidget {
   const ECGMonitoringHomeScreen(this._user, {super.key});
@@ -39,12 +43,115 @@ class _ECGMonitoringHomeScreenState extends State<ECGMonitoringHomeScreen> {
   late double responsiveButtonLength = _width / (_hDevWidth / buttonLength);
   late double responsiveButtonRoundness =
       _width / (_hDevWidth / buttonRoundness);
+  List<Map<String, dynamic>> _lastEcgData = [];
+  bool _lastEcgIsLoading = true;
 
   @override
   void initState() {
     super.initState();
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+    _getLastEcg();
+  }
+
+  Future<void> _getLastEcg() async {
+    _lastEcgData = await EcgService.getLastEcg(widget._user.email);
+    setState(() {
+      _lastEcgIsLoading = false;
+    });
+  }
+
+  Widget _ecgPlot(
+      List<double> data, double minValue, double maxValue, Color color) {
+    return Padding(
+      padding: EdgeInsets.only(
+          top: _height / (_devHeight / 1),
+          left: _width / (_devWidth / 16),
+          right: _width / (_devWidth / 16),
+          bottom: _height / (_devHeight / 16)),
+      child: IgnorePointer(
+        ignoring: true,
+        child: LineChart(
+          LineChartData(
+            lineBarsData: [
+              LineChartBarData(
+                spots: List.generate(
+                  data.length,
+                  (index) => FlSpot(index.toDouble(), data[index]),
+                ),
+                isCurved: false,
+                colors: [color],
+                barWidth: 2,
+                isStrokeCapRound: true,
+                dotData: FlDotData(
+                  show: false,
+                ),
+              ),
+            ],
+            minY: minValue,
+            // Adjust these values based on your data range
+            maxY: maxValue,
+            titlesData: FlTitlesData(
+              bottomTitles: SideTitles(
+                showTitles: true,
+                getTitles: (value) {
+                  if (value % 500 == 0) {
+                    return (value ~/ 500).toString();
+                  } else {
+                    return "";
+                  }
+                },
+              ),
+              leftTitles: SideTitles(
+                showTitles: true,
+              ),
+            ),
+            borderData: FlBorderData(
+              show: true,
+              border: Border.all(color: Colors.black),
+            ),
+            gridData: FlGridData(
+              show: true,
+              drawHorizontalLine: true,
+              drawVerticalLine: true,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  double _calcMin(List<double> data) {
+    double minValue =
+        data.reduce((value, element) => value < element ? value : element) -
+            0.1;
+    return minValue;
+  }
+
+  double _calcMax(List<double> data) {
+    double maxValue =
+        data.reduce((value, element) => value > element ? value : element) +
+            0.1;
+    return maxValue;
+  }
+
+  Widget _lead1Plot(List<double> l1Data) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: _height / (_devHeight / 25)),
+          child: Text(
+            "Lead I",
+            style: TextStyle(fontSize: _width / (_devWidth / 10)),
+          ),
+        ),
+        SizedBox(
+            height: _height / (_devHeight / 130),
+            child: _ecgPlot(
+                l1Data, _calcMin(l1Data), _calcMax(l1Data), Colors.blue)),
+      ],
+    );
   }
 
   void _onClickHistoryBtn() {
@@ -142,18 +249,47 @@ class _ECGMonitoringHomeScreenState extends State<ECGMonitoringHomeScreen> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          'ECG Graphical Illustrations',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
+                        Row(
+                          children: [
+                            Text(
+                              'Recent ECG Report         ',
+                              style: TextStyle(
+                                  fontSize: _height / (_devHeight / 10),
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            _lastEcgData.length != 0
+                                ? Text(
+                                    'Date: ${DateFormat('yyyy-MM-dd').format(_lastEcgData[0]["datetime"].toDate())}         Time: ${DateFormat('HH:mm:ss').format(_lastEcgData[0]["datetime"].toDate())}',
+                                    style: TextStyle(
+                                      fontSize: _height / (_devHeight / 10),
+                                    ),
+                                  )
+                                : SizedBox()
+                          ],
                         ),
                         SizedBox(height: 8),
                         Container(
                           height: _height / (_devHeight / 180),
+                          width: _width,
                           color: Colors.blueGrey[50], // Placeholder for graph
-                          child: Center(
-                            child: Text('Graph Placeholder'),
-                          ),
+                          child: _lastEcgIsLoading == true
+                              ? Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    CircularProgressIndicator(),
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                          top: _height / (_devHeight / 8)),
+                                      child: Text('Loading'),
+                                    ),
+                                  ],
+                                )
+                              : _lastEcgData.length == 0
+                                  ? Center(
+                                      child: Text('No ECG Report'),
+                                    )
+                                  : _lead1Plot(
+                                      _lastEcgData[0]["l1"].cast<double>()),
                         ),
                       ],
                     ),
