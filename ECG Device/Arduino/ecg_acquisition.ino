833039b931ec9e57b0eb935378b9b0c85102f15e
Sathira-Wijeratne <swije27@gmail.com>
2024-08-29 12:40:53 +0530

    implemented serial communication and pin monitoring for lead detection and analog input

--- ECG Device/Arduino/ecg_acquisition.ino
+++ ECG Device/Arduino/ecg_acquisition.ino
@@ -1,30 +1,8 @@
-void setup() {
-  // initialize the serial communication:
-  Serial.begin(9600);
-  pinMode(10, INPUT); // Setup for leads off detection LO +
-  pinMode(11, INPUT); // Setup for leads off detection LO -
-
-}
-
-void loop() {
-  if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
-    Serial.println('!');
-  }
-  else{
-    // send the value of analog input 0:
-    Serial.println(analogRead(A0));
-  }
-  //Wait for a bit to keep serial data from saturating
-  delay(1);
-}
-
-
 // void setup() {
 //   // initialize the serial communication:
 //   Serial.begin(9600);
 //   pinMode(10, INPUT); // Setup for leads off detection LO +
 //   pinMode(11, INPUT); // Setup for leads off detection LO -
-//
 // }
 //
 // void loop() {
@@ -38,9 +16,3 @@ void loop() {
 //   //Wait for a bit to keep serial data from saturating
 //   delay(1);
 // }
-
-
-
-
-
-
