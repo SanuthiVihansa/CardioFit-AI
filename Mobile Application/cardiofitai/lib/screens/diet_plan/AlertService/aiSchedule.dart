69309667798793714e21533ea5f8fe3c90604ae8
SanuthiVihansa <subasofts@gmail.com>
2024-10-14 20:56:44 +0530

    Create Ai Schedule page

---
+++ Mobile Application/cardiofitai/lib/screens/diet_plan/AlertService/aiSchedule.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AISchedule extends StatefulWidget {
+  const AISchedule({super.key});
+
+  @override
+  State<AISchedule> createState() => _AIScheduleState();
+}
+
+class _AIScheduleState extends State<AISchedule> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
