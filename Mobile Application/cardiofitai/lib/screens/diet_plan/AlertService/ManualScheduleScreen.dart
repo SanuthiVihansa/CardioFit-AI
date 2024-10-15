66f5d0307d404405395950bd03d3c27da3497506
SanuthiVihansa <subasofts@gmail.com>
2024-10-15 20:56:12 +0530

    create stateful method

---
+++ Mobile Application/cardiofitai/lib/screens/diet_plan/AlertService/ManualScheduleScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ManualScheduleScreen extends StatefulWidget {
+  const ManualScheduleScreen({super.key});
+
+  @override
+  State<ManualScheduleScreen> createState() => _ManualScheduleScreenState();
+}
+
+class _ManualScheduleScreenState extends State<ManualScheduleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
