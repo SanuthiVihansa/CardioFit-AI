77f8989cd9a7484461e88c9c2e6853ea2d3785ed
Sathira-Wijeratne <swije27@gmail.com>
2024-10-25 13:11:40 +0530

    added meaningful response for user

--- Mobile Application/cardiofitai/lib/screens/explainable_AI/model_feature_showcase_screen_for_shap.dart
+++ Mobile Application/cardiofitai/lib/screens/explainable_AI/model_feature_showcase_screen_for_shap.dart
@@ -20,7 +20,8 @@ class ECGPlotScreen extends StatefulWidget {
 class _ECGPlotScreenState extends State<ECGPlotScreen> {
   late double _width, _height;
   bool _isLoading = true;
-  String _errorMessage = '', _selectedLead = "Lead I"; //this has to change because Lead I may not always be there
+  String _errorMessage = '', _selectedLead = '', _disease = '', _conclusion = '';
+  List<String> _doctorLeads = [], _modelLeads = [];
   final double _devWidth = 753.4545454545455, _devHeight = 392.72727272727275;
   Map<String, List<double>> ecgData12Lead = {};
   List<Map<String, dynamic>> filteredShapValues = [];
@@ -53,6 +54,13 @@ class _ECGPlotScreenState extends State<ECGPlotScreen> {
     if (response.statusCode == 200) {
       final responseData = jsonDecode(response.body);
       setState(() {
+        _selectedLead = responseData['model_leads'].isNotEmpty ? responseData['model_leads'].first : 'No Leads';
+        _dropDownMenuItems.clear();
+        _dropDownMenuItems.addAll(List<String>.from(responseData['model_leads']));
+        _disease = responseData['disease'];
+        _conclusion = responseData['conclusion'];
+        _doctorLeads = responseData['doctor_leads'] == '' ? [] : List<String>.from(responseData['doctor_leads']);
+        _modelLeads = List<String>.from(responseData['model_leads']);
         filteredShapValues = List<Map<String, dynamic>>.from(responseData['filtered_shap_values']); _isLoading = false;
       });
     } else {
@@ -79,8 +87,8 @@ class _ECGPlotScreenState extends State<ECGPlotScreen> {
               const CircularProgressIndicator()
             ]) :
             _errorMessage.isNotEmpty ? Padding(
-                padding: const EdgeInsets.all(20.0), child: Text(_errorMessage, style: TextStyle(fontSize: _width / (_devWidth / 20), color: Colors.red))) :
-            Padding(
+                padding: const EdgeInsets.all(20.0), child: Text(_errorMessage, style: TextStyle(fontSize: _width / (_devWidth / 20), color: Colors.red)))
+                : Padding(
                 padding: EdgeInsets.only(top: _height / (_devHeight / 16), left: _width / (_devWidth / 16), right: _width / (_devWidth / 16), bottom: _height / (_devHeight / 16)),
                 child: Column(children: [
                   Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
@@ -93,7 +101,40 @@ class _ECGPlotScreenState extends State<ECGPlotScreen> {
                   Expanded(
                       child: SizedBox(
                           height: _height / (_devHeight / 200), child: _selectedLeadPlot())
-                  )])
+                  ),
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    RichText(
+                      text: TextSpan(
+                        text: 'Disease: ',
+                        style: TextStyle(fontSize: _width / (_devWidth / 10), fontWeight: FontWeight.bold, color: Colors.black),
+                        children: [
+                          TextSpan(
+                            text: _disease,
+                            style: const TextStyle(fontWeight: FontWeight.normal),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]),
+                  _doctorLeads.isNotEmpty ? Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    RichText(
+                      text: TextSpan(
+                        text: 'Doctor Leads: ',
+                        style: TextStyle(fontSize: _width / (_devWidth / 10), fontWeight: FontWeight.bold, color: Colors.black),
+                        children: [
+                          TextSpan(
+                            text: _doctorLeads.join(', '),
+                            style: const TextStyle(fontWeight: FontWeight.normal),
+                          ),],),),]) : const SizedBox.shrink(),
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    RichText(text: TextSpan(text: 'Model Leads: ', style: TextStyle(fontSize: _width / (_devWidth / 10), fontWeight: FontWeight.bold, color: Colors.black),
+                        children: [TextSpan(text: _modelLeads.join(', '), style: const TextStyle(fontWeight: FontWeight.normal),)])),
+                  ]),
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    RichText(text: TextSpan(text: 'Conclusion: ', style: TextStyle(fontSize: _width / (_devWidth / 10), fontWeight: FontWeight.bold, color: Colors.black),
+                        children: [TextSpan(text: _conclusion, style: const TextStyle(fontWeight: FontWeight.normal),)])),
+                  ]),
+                ])
             )
         )
     );
@@ -117,20 +158,21 @@ class _ECGPlotScreenState extends State<ECGPlotScreen> {
     String transformedKey = _ecgLeadKeys[_dropDownMenuItems.indexOf(_selectedLead)];
     List<double> leadData = ecgData12Lead[transformedKey] ?? [];
     Map<String, dynamic> shapValues = filteredShapValues.firstWhere((element) => element['lead'] == _dropDownMenuItems.indexOf(_selectedLead) + 1, orElse: () => {});
-    return leadData.isNotEmpty ? _ecgPlotWithFocus(leadData, shapValues, leadData.reduce((a, b) => a < b ? a : b), leadData.reduce((a, b) => a > b ? a : b), Colors.blue) : const Center(child: Text('No data available for the selected lead.'));
+    return leadData.isNotEmpty ? _ecgPlotWithFocus(leadData, shapValues, leadData.reduce((a, b) => a < b ? a : b), leadData.reduce((a, b) => a > b ? a : b), Colors.red, Colors.blue) : const Center(child: Text('No data available for the selected lead.'));
   }
 
-  Widget _ecgPlotWithFocus(List<double> data, Map<String, dynamic> shapValues, double minValue, double maxValue, Color color) {
+  Widget _ecgPlotWithFocus(List<double> data, Map<String, dynamic> shapValues, double minValue, double maxValue, Color color, Color focusPointColor) {
     List<int> importantTimePoints = shapValues.isNotEmpty ? List<int>.from(shapValues['important_time_points']) : [];
+    double opacity = 1;
     return Padding(
-        padding: EdgeInsets.only(top: _height / (_devHeight / 1), left: _width / (_devWidth / 16), right: _width / (_devWidth / 16), bottom: _height / (_devHeight / 16)),
+        padding: EdgeInsets.only(top: _height / (_devHeight / 10), left: _width / (_devWidth / 16), right: _width / (_devWidth / 16), bottom: _height / (_devHeight / 16)),
         child: IgnorePointer(
             ignoring: true, child: LineChart(LineChartData(
             lineBarsData: [
               LineChartBarData(
                   spots: List.generate(data.length, (index) => FlSpot(index.toDouble(), data[index])),
                   isCurved: false, colors: [color], barWidth: 2, isStrokeCapRound: true,
-                  dotData: FlDotData(show: true, checkToShowDot: (spot, _) => importantTimePoints.contains(spot.x.toInt())))
+                  dotData: FlDotData(show: true, getDotPainter: (spot, _, __, ___) => FlDotCirclePainter(strokeColor: focusPointColor, radius: 0.5, color: focusPointColor,),checkToShowDot: (spot, _) => importantTimePoints.contains(spot.x.toInt()))),
             ],
             minY: minValue, maxY: maxValue,
             titlesData: FlTitlesData(
@@ -138,30 +180,5 @@ class _ECGPlotScreenState extends State<ECGPlotScreen> {
                 leftTitles: SideTitles(showTitles: true)),
             borderData: FlBorderData(show: true, border: Border.all(color: Colors.black)),
             gridData: FlGridData(show: true, drawHorizontalLine: true, drawVerticalLine: true)))));
-  }
-}
-
-class ECGLeadChart extends StatelessWidget {
-  final List<double> ecgData;
-  final Map<String, dynamic> shapValues;
-
-  const ECGLeadChart({required this.ecgData, required this.shapValues, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    List<int> importantTimePoints = shapValues.isNotEmpty ? List<int>.from(shapValues['important_time_points']) : [];
-    return LineChart(LineChartData(
-        gridData: FlGridData(show: false),
-        titlesData: FlTitlesData(show: false),
-        borderData: FlBorderData(show: false),
-        lineBarsData: [
-          LineChartBarData(
-              spots: ecgData.asMap().entries.map((entry) {
-                int index = entry.key; double value = entry.value;
-                return FlSpot(index.toDouble(), value);
-              }).toList(),
-              isCurved: false, colors: [Colors.blue],
-              dotData: FlDotData(show: true, checkToShowDot: (spot, _) => importantTimePoints.contains(spot.x.toInt())))
-        ]));
   }
 }
