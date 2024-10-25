e65b13c04d5d068b6c27e6878cf20feb83d525b0
Sathira-Wijeratne <swije27@gmail.com>
2024-10-25 14:42:05 +0530

    finalize color schemes and information for users understanding

--- Mobile Application/cardiofitai/lib/screens/explainable_AI/model_feature_showcase_screen_for_shap.dart
+++ Mobile Application/cardiofitai/lib/screens/explainable_AI/model_feature_showcase_screen_for_shap.dart
@@ -18,6 +18,7 @@ class ECGPlotScreen extends StatefulWidget {
 }
 
 class _ECGPlotScreenState extends State<ECGPlotScreen> {
+  int userInfoFontSize = 17;
   late double _width, _height;
   bool _isLoading = true;
   String _errorMessage = '', _selectedLead = '', _disease = '', _conclusion = '';
@@ -27,6 +28,7 @@ class _ECGPlotScreenState extends State<ECGPlotScreen> {
   List<Map<String, dynamic>> filteredShapValues = [];
   final List<String> _ecgLeadKeys = ['l1', 'l2', 'l3', 'avr', 'avl', 'avf', 'v1', 'v2', 'v3', 'v4', 'v5', 'v6'];
   final List<String> _dropDownMenuItems = ['Lead I', 'Lead II', 'Lead III', 'Lead aVR', 'Lead aVL', 'Lead aVF', 'Lead V1', 'Lead V2', 'Lead V3', 'Lead V4', 'Lead V5', 'Lead V6'];
+  final String _upBaseLineServerUrl = 'http://swije.pythonanywhere.com/load/model';
 
   @override
   void initState() {
@@ -34,8 +36,16 @@ class _ECGPlotScreenState extends State<ECGPlotScreen> {
     loadECGData();
   }
 
+  Future<void> upBaseLineServerAndLoadTrainData() async {
+    await http.get(Uri.parse(_upBaseLineServerUrl), headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    });
+  }
+
   Future<void> loadECGData() async {
     try {
+      upBaseLineServerAndLoadTrainData();
+
       String jsonString = await widget.ecgFile.readAsString();
       ecgData12Lead = (jsonDecode(jsonString) as Map<String, dynamic>).map((key, value) => MapEntry(key, List<double>.from(value)));
       sendECGData(ecgData12Lead);
@@ -60,7 +70,10 @@ class _ECGPlotScreenState extends State<ECGPlotScreen> {
         _disease = responseData['disease'];
         _conclusion = responseData['conclusion'];
         _doctorLeads = responseData['doctor_leads'] == '' ? [] : List<String>.from(responseData['doctor_leads']);
-        _modelLeads = List<String>.from(responseData['model_leads']);
+        _modelLeads = List<String>.from(responseData['model_leads'])
+            .map((lead) => lead.replaceAll('Lead ', ''))
+            .toList();
+
         filteredShapValues = List<Map<String, dynamic>>.from(responseData['filtered_shap_values']); _isLoading = false;
       });
     } else {
@@ -78,7 +91,7 @@ class _ECGPlotScreenState extends State<ECGPlotScreen> {
 
     return Scaffold(
         appBar: AppBar(
-            title: const Text('ECG Plot with Model Focus', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
+            title: const Text('Diagnosis focused features', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
             foregroundColor: Colors.white, backgroundColor: Colors.red),
         body: Center(
             child: _isLoading ? Column(
@@ -88,54 +101,54 @@ class _ECGPlotScreenState extends State<ECGPlotScreen> {
             ]) :
             _errorMessage.isNotEmpty ? Padding(
                 padding: const EdgeInsets.all(20.0), child: Text(_errorMessage, style: TextStyle(fontSize: _width / (_devWidth / 20), color: Colors.red)))
-                : Padding(
-                padding: EdgeInsets.only(top: _height / (_devHeight / 16), left: _width / (_devWidth / 16), right: _width / (_devWidth / 16), bottom: _height / (_devHeight / 16)),
-                child: Column(children: [
-                  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                    Text("ECG Results", style: TextStyle(fontWeight: FontWeight.bold, fontSize: _width / (_devWidth / 20))),
-                    Row(children: [
-                      Text("Selected Lead: ", style: TextStyle(fontSize: _width / (_devWidth / 14))),
-                      _leadSelectionDropDown()
-                    ])
-                  ]),
-                  Expanded(
-                      child: SizedBox(
-                          height: _height / (_devHeight / 200), child: _selectedLeadPlot())
-                  ),
-                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                    RichText(
-                      text: TextSpan(
-                        text: 'Disease: ',
-                        style: TextStyle(fontSize: _width / (_devWidth / 10), fontWeight: FontWeight.bold, color: Colors.black),
-                        children: [
-                          TextSpan(
-                            text: _disease,
-                            style: const TextStyle(fontWeight: FontWeight.normal),
-                          ),
-                        ],
+                : SingleChildScrollView(
+                  child: Padding(
+                  padding: EdgeInsets.only(top: _height / (_devHeight / 16), left: _width / (_devWidth / 16), right: _width / (_devWidth / 16), bottom: _height / (_devHeight / 16)),
+                  child: Column(children: [
+                    Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                      Text("ECG Results", style: TextStyle(fontWeight: FontWeight.bold, fontSize: _width / (_devWidth / 20))),
+                      Row(children: [
+                        Text("Selected Lead: ", style: TextStyle(fontSize: _width / (_devWidth / 14))),
+                        _leadSelectionDropDown()
+                      ])
+                    ]),
+                    SizedBox(
+                        height: _height / (_devHeight / 200), child: _selectedLeadPlot()),
+                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                      RichText(
+                        text: TextSpan(
+                          text: 'Disease diagnosis: ',
+                          style: TextStyle(fontSize: _width / (_devWidth / 15), fontWeight: FontWeight.bold, color: Colors.black),
+                          children: [
+                            TextSpan(
+                              text: _disease,
+                              style: const TextStyle(fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ]),
-                  _doctorLeads.isNotEmpty ? Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                    RichText(
-                      text: TextSpan(
-                        text: 'Doctor Leads: ',
-                        style: TextStyle(fontSize: _width / (_devWidth / 10), fontWeight: FontWeight.bold, color: Colors.black),
-                        children: [
-                          TextSpan(
-                            text: _doctorLeads.join(', '),
-                            style: const TextStyle(fontWeight: FontWeight.normal),
-                          ),],),),]) : const SizedBox.shrink(),
-                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                    RichText(text: TextSpan(text: 'Model Leads: ', style: TextStyle(fontSize: _width / (_devWidth / 10), fontWeight: FontWeight.bold, color: Colors.black),
-                        children: [TextSpan(text: _modelLeads.join(', '), style: const TextStyle(fontWeight: FontWeight.normal),)])),
-                  ]),
-                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                    RichText(text: TextSpan(text: 'Conclusion: ', style: TextStyle(fontSize: _width / (_devWidth / 10), fontWeight: FontWeight.bold, color: Colors.black),
-                        children: [TextSpan(text: _conclusion, style: const TextStyle(fontWeight: FontWeight.normal),)])),
-                  ]),
-                ])
-            )
+                    ]),
+                    _doctorLeads.isNotEmpty ? Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                      RichText(
+                        text: TextSpan(
+                          text: 'Doctor Leads: ',
+                          style: TextStyle(fontSize: _width / (_devWidth / 15), fontWeight: FontWeight.bold, color: Colors.black),
+                          children: [
+                            TextSpan(
+                              text: _doctorLeads.join(', '),
+                              style: const TextStyle(fontWeight: FontWeight.normal),
+                            ),],),),]) : const SizedBox.shrink(),
+                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                      RichText(text: TextSpan(text: 'Focused ECG leads: ', style: TextStyle(fontSize: _width / (_devWidth / 15), fontWeight: FontWeight.bold, color: Colors.black),
+                          children: [TextSpan(text: _modelLeads.join(', '), style: const TextStyle(fontWeight: FontWeight.normal),)])),
+                    ]),
+                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                      RichText(text: TextSpan(text: 'Conclusion: ', style: TextStyle(fontSize: _width / (_devWidth / 15), fontWeight: FontWeight.bold, color: Colors.black),
+                          children: [TextSpan(text: _conclusion, style: const TextStyle(fontWeight: FontWeight.normal),)])),
+                    ]),
+                  ])
+                              ),
+                )
         )
     );
   }
@@ -158,7 +171,7 @@ class _ECGPlotScreenState extends State<ECGPlotScreen> {
     String transformedKey = _ecgLeadKeys[_dropDownMenuItems.indexOf(_selectedLead)];
     List<double> leadData = ecgData12Lead[transformedKey] ?? [];
     Map<String, dynamic> shapValues = filteredShapValues.firstWhere((element) => element['lead'] == _dropDownMenuItems.indexOf(_selectedLead) + 1, orElse: () => {});
-    return leadData.isNotEmpty ? _ecgPlotWithFocus(leadData, shapValues, leadData.reduce((a, b) => a < b ? a : b), leadData.reduce((a, b) => a > b ? a : b), Colors.red, Colors.blue) : const Center(child: Text('No data available for the selected lead.'));
+    return leadData.isNotEmpty ? _ecgPlotWithFocus(leadData, shapValues, leadData.reduce((a, b) => a < b ? a : b), leadData.reduce((a, b) => a > b ? a : b), Colors.blue, Colors.redAccent) : const Center(child: Text('No data available for the selected lead.'));
   }
 
   Widget _ecgPlotWithFocus(List<double> data, Map<String, dynamic> shapValues, double minValue, double maxValue, Color color, Color focusPointColor) {
@@ -172,7 +185,7 @@ class _ECGPlotScreenState extends State<ECGPlotScreen> {
               LineChartBarData(
                   spots: List.generate(data.length, (index) => FlSpot(index.toDouble(), data[index])),
                   isCurved: false, colors: [color], barWidth: 2, isStrokeCapRound: true,
-                  dotData: FlDotData(show: true, getDotPainter: (spot, _, __, ___) => FlDotCirclePainter(strokeColor: focusPointColor, radius: 0.5, color: focusPointColor,),checkToShowDot: (spot, _) => importantTimePoints.contains(spot.x.toInt()))),
+                  dotData: FlDotData(show: true, getDotPainter: (spot, _, __, ___) => FlDotCirclePainter(strokeColor: focusPointColor, radius: 0.4, color: focusPointColor,),checkToShowDot: (spot, _) => importantTimePoints.contains(spot.x.toInt()))),
             ],
             minY: minValue, maxY: maxValue,
             titlesData: FlTitlesData(
